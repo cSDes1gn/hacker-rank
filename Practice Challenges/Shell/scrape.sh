5410c2409962bb3040f5141d7c7df4f823adf63b
cSDes1gn <christian911@sympatico.ca>
2020-07-05 11:52:10 -0400

    avoiding word splitting with quotes

--- Practice Challenges/Shell/scrape.sh
+++ Practice Challenges/Shell/scrape.sh
@@ -1,5 +1,5 @@
 #! /bin/bash
 
-host_id=$( echo $(</Users/christiansargusingh/Downloads/uuid.txt) | cut -c1-4 )
-prefix="IRIS-"
+host_id=$( echo "$(</Users/christiansargusingh/Downloads/uuid.txt)" | cut -c1-4 )
+prefix="HOST-"
 echo "$prefix$host_id"
